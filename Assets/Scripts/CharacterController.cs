42a6680a8856c1da79ea5b9876ce86e9e2c51673
Pearce <ppachuta@gmail.com>
2024-07-02 20:51:32 -0700

    camera-controller separation

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CharacterController : MonoBehaviour
+{
+
+    private GameObject plr;
+    private Rigidbody rb;
+
+    private Vector3 targetFacing;
+    private Vector3 targetMovement;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        plr = gameObject;
+        rb = GetComponent<Rigidbody>();
+    }
+
+    public GameObject getObject()
+    {
+        return this.plr;
+    }
+
+    public Rigidbody getRigidbody()
+    {
+        return this.rb;
+    }
+
+    public void setMovement(Vector3 movementInput)
+    {
+        this.targetMovement = movementInput;
+    }
+
+    public void setTargetFacing(Vector3 targetFacing)
+    {
+        this.targetFacing = targetFacing;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        ///SUMMARY: A simple character controller leveraging the Unity Rigidbody system and direct velocity control.
+        ///A system using addForce and considering actual shoe-to-field contact surface area, materials, and relative friction would be much more realistic, but out-of-scope for a demo like this to make feel right.
+        ///We have much more direct control over how the player feels this way.
+    
+
+
+
+        //Transform player input into player-local coordinate system
+        Vector3 inputTransform = plr.transform.TransformVector(targetMovement);
+
+        //Add input to the player's velocity
+        rb.velocity += new Vector3(inputTransform.x, 0, inputTransform.z);
+
+        //Apply drag from the square of velocity times some number (Shooting for "feels good" here)
+        rb.velocity -= rb.velocity * rb.velocity.magnitude * 0.005f;
+
+        //Rotate the player toward their camera direction
+        //Favor a torque solution rather than a setting solution because it's a bit more stable for less work.
+        float deltaAngle = Vector3.SignedAngle(plr.transform.forward, targetFacing, Vector3.up) / 100.0f;
+        rb.AddTorque(new Vector3(0, deltaAngle * 5.0f, 0) + new Vector3(0, rb.angularVelocity.y * -1.0f, 0), 0);
+
+    }
+
+    private void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/PlayerInputHandler.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInputHandler : MonoBehaviour
+{
+    private Control control;
+    private float cameraYawAccumulator = 0;
+    private float cameraHeightAccumulator = 1.25f;
+
+
+    [SerializeField]
+    private GameObject playerCamera;
+
+    [SerializeField]
+    private CharacterController controller;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        control = new Control();
+        control.Enable();
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        //Control input gathering. It would be more efficient to do this with callbacks, but for this application the differences are marginal
+        //Obtain player input as a Vector. Vector3 so we don't have to mess with y vs z later.
+        Vector3 inputVector = new Vector3(control.Base.Movement.ReadValue<Vector2>().x, 0, control.Base.Movement.ReadValue<Vector2>().y) * 0.4f;
+
+        //Obtain player mouse input for camera control
+        Vector2 mouseInput = control.Base.CameraRotation.ReadValue<Vector2>();
+        cameraYawAccumulator += mouseInput.x / 100.0f;
+        cameraHeightAccumulator += mouseInput.y / -100.0f;
+        cameraHeightAccumulator = Mathf.Clamp(cameraHeightAccumulator, -0.5f, 3.0f);
+        cameraYawAccumulator %= 6.28f;
+
+        controller.setMovement(inputVector);
+
+        if (playerCamera != null)
+        {
+            float cameraDistance = 4;
+
+            //A little bit of running camera shake, for fun.
+            //We sample perlin at the player's position and bump the camera along the perlin
+            //We do the same thing for camera roll, but at an offset so the bumping and rolling do not appear coupled.
+            Transform plrTransform = controller.getObject().transform;
+            Rigidbody plrRB = controller.getRigidbody();
+            float cameraShake = ((Mathf.PerlinNoise(plrTransform.position.x, plrTransform.position.z) - 0.5f) / 36.0f) * plrRB.velocity.magnitude;
+            float cameraTumble = ((Mathf.PerlinNoise(plrTransform.position.x + 1500, plrTransform.position.z + 1500) - 0.5f) / 5.0f) * plrRB.velocity.magnitude;
+
+            playerCamera.transform.position = plrTransform.position + new Vector3(Mathf.Sin(cameraYawAccumulator) * cameraDistance, cameraHeightAccumulator + cameraShake, Mathf.Cos(cameraYawAccumulator) * cameraDistance);
+            playerCamera.transform.LookAt(plrTransform.position + new Vector3(0, 1, 0) + (plrRB.velocity / 10.0f));
+            playerCamera.transform.Rotate(Vector3.forward, cameraTumble, Space.Self);
+
+            controller.setTargetFacing(playerCamera.transform.forward);
+        }
+    }
+}
